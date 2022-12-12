0ed778eb9d07b3e8a05457c18fd731d5125435f2
ArtemLevin <python_study_2022@mail.ru>
2022-12-12 15:13:44 +0300

    FindTheElementInMassive

---
+++ Massive.1.2.2/Program.cs
@@ -0,0 +1,44 @@
+﻿int [] CreateArray(int number)
+{
+    int [] array = new int[number];
+    int i = 0;
+    while (i < number)
+    {
+        Console.Write("Enter a number ");
+        array[i] = int.Parse(Console.ReadLine()!);
+        i ++;
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write(array[i] + " ");
+        i++;
+    } 
+}
+
+Console.Write("Enter masiive length ");
+int number = int.Parse(Console.ReadLine()!);
+
+// PrintArray(CreateArray(number));
+
+int NumberOfElements(int [] array, int k)
+{
+    int i = 0; 
+    int j = 0;
+    while(i < array.Length)
+    {
+        if (array[i] == k)
+        {
+            j++;
+        }
+        i++;
+    }
+    return j;
+}
+
+Console.Write($"Found {NumberOfElements(CreateArray(number), 0)} elements of massive equal 0");
