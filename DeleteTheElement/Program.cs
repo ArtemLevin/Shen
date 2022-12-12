e7630689e9993bb156c2eb4588f0cd37a0f6f370
ArtemLevin <python_study_2022@mail.ru>
2022-12-13 01:21:51 +0300

    Delete the element

---
+++ DeleteTheElement/Program.cs
@@ -0,0 +1,50 @@
+﻿int[] CreateArray(int number)
+{
+    int[] array = new int[number];
+    int i = 0;
+    while (i < number)
+    {
+        Console.Write("Enter a number ");
+        array[i] = int.Parse(Console.ReadLine()!);
+        i++;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write(array[i] + " ");
+        i++;
+    }
+}
+
+Console.Write("Enter massive length ");
+int number = int.Parse(Console.ReadLine()!);
+
+int[] arrayCopied = new int[number];
+arrayCopied = CreateArray(number);
+
+PrintArray(arrayCopied);
+
+Console.WriteLine(" ");
+
+int[] arrayHelp = new int[arrayCopied.Length -1];
+int p = 0;
+int j = 0;
+while (p < arrayCopied.Length-1)
+    {
+        if(j == 3)
+        {
+            j++;
+        }
+        arrayHelp[p] = arrayCopied[j];
+        p++;
+        j++;
+    } 
+arrayCopied = arrayHelp; 
+PrintArray(arrayHelp);
+Console.WriteLine(" ");
+PrintArray(arrayCopied);  
