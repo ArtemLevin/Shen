cca8d6af3bafab9149850d6734599e57fbc1da28
ArtemLevin <python_study_2022@mail.ru>
2022-12-12 15:36:51 +0300

    ArrayCopy

---
+++ Massive.1.2.3/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int [] CreateArray(int number)
+{
+    int [] array = new int[number];
+    int i = 0;
+    while (i < number)
+    {
+        Console.Write("Enter a number ");
+        array[i] = int.Parse(Console.ReadLine()!);
+        i ++;
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write(array[i] + " ");
+        i++;
+    } 
+}
+Console.Write("Enter massive length ");
+int number = int.Parse(Console.ReadLine()!);
+
+int [] arrayCopied = new int[number];
+arrayCopied = CreateArray(number);
+
+PrintArray(arrayCopied);
