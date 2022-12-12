da440418de2ec0ae72bd6b67ea586fa50ff4e2c8
ArtemLevin <python_study_2022@mail.ru>
2022-12-12 23:56:39 +0300

    NOT FIXED The number of different elements

---
+++ MassiveDifferentElements/Program.cs
@@ -0,0 +1,53 @@
+﻿int[] CreateArray(int number)
+{
+    int[] array = new int[number];
+    int i = 0;
+    while (i < number)
+    {
+        Console.Write("Enter a number ");
+        array[i] = int.Parse(Console.ReadLine()!);
+        i++;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write(array[i] + " ");
+        i++;
+    }
+}
+
+Console.Write("Enter massive length ");
+int number = int.Parse(Console.ReadLine()!);
+
+int[] arrayCopied = new int[number];
+arrayCopied = CreateArray(number);
+
+PrintArray(arrayCopied);
+
+int DifferentElements(int[] array)
+{
+    int i = 0;
+    int k = 0;
+    int dif;
+    while (i < array.Length)
+    {
+        int j = i + 1;
+        while (j < array.Length)
+        {
+            if (array[j] == array[i])
+            {
+                k++;
+            }
+            j++;
+        }
+        i++;
+    }
+    return dif = array.Length - k + 1;
+}
+
+Console.Write(" The number of different elements: " + DifferentElements(arrayCopied));
