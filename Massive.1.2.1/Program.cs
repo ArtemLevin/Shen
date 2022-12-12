d2d606674c9080e675bfb66f55ec8db2015e2263
ArtemLevin <python_study_2022@mail.ru>
2022-12-12 15:04:44 +0300

    1.2.1

---
+++ Massive.1.2.1/Program.cs
@@ -0,0 +1,39 @@
+﻿int [] CreateArray(int number)
+{
+    int [] array = new int[number];
+    int i = 0;
+    while (i < number)
+    {
+        Console.Write("Enter a number ");
+        array[i] = int.Parse(Console.ReadLine()!);
+        i ++;
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write(array[i] + " ");
+        i++;
+    } 
+}
+Console.Write("Enter masiive length ");
+int number = int.Parse(Console.ReadLine()!);
+
+// PrintArray(CreateArray(number));
+
+int [] ZeroArray(int [] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        array[i] = 0;
+        i++;
+    } 
+    return array;
+}
+
+PrintArray(ZeroArray(CreateArray(number)));
