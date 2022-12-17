70889cb8aca1c7966088514b5e25ebd0629aa981
ArtemLevin <python_study_2022@mail.ru>
2022-12-18 01:00:37 +0300

    DecimalToBinary

---
+++ BinaryToDecimal/Program.cs
@@ -0,0 +1,19 @@
+﻿// // String res = Format.Empty;
+// int num = 123;
+string Binary(int num)
+{
+    if (num == 0)
+    {
+        return "0";
+    }
+    if (num == 1)
+    {
+        return "1";
+    }
+    else
+    {
+        
+        return  Binary(num/2) + num%2;
+    }
+} 
+Console.WriteLine(Binary(121));
