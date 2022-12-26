322432e7b23ac0c2292aa1ab625572af2fd9d1f8
ArtemLevin <python_study_2022@mail.ru>
2022-12-26 21:49:46 +0300

    MirrorTansp

---
+++ Popov.70.13/Program.cs
@@ -0,0 +1,55 @@
+﻿// Дана квадратная матрица N×N, состоящая из натуральных чисел. 
+// Зеркально отразить ее элементы относительно побочной диагонали. Вывести
+// результат на экран.
+
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0}	", array[i, j]);
+        }
+        Console.WriteLine(" ");
+    }
+}
+Console.Write("Enter the number or rows...");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number or columns..");
+int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine(" ");
+int[,] array = new int[rows, columns];
+array = CreateArray(rows, columns);
+Console.WriteLine(" ");
+PrintArray(array);
+Console.WriteLine(" ");
+
+
+int[,] TransportingElements(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - i; j++)
+        {
+            temp = array[i, j];
+            array[i, j] = array[array.GetLength(0) - j - 1, array.GetLength(1) - i - 1];
+            array[array.GetLength(0) - j - 1, array.GetLength(1) - i - 1] = temp;
+        }
+    }
+    return array;
+}
+
+PrintArray(TransportingElements(array));
